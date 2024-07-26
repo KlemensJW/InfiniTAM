ec36c6b9fcb412d10d97782d4ea99ab9398295f9
Klemens_Ubuntu <klemens.wiesinger@outlook.at>
2024-07-26 13:14:00 +0200

    trying to get MAVSDK to run

--- InfiniTAM/Apps/InfiniTAM_drone/DroneEngine.cpp
+++ InfiniTAM/Apps/InfiniTAM_drone/DroneEngine.cpp
@@ -6,14 +6,76 @@
 
 #include "../../ITMLib/Core/ITMMainEngine.h"
 #include <iostream>
+#include <memory>
 #include <thread>
+#include <mavsdk/plugins/action/action.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+#include <mavsdk/plugins/offboard/offboard.h>
+
 
 DroneEngine* DroneEngine::instance;
+
+DroneEngine::DroneEngine()
+    : mavsdk(mavsdk::Mavsdk::Configuration{mavsdk::Mavsdk::ComponentType::GroundStation})
+{
+
+}
 
 void DroneEngine::Initialise(ITMLib::ITMMainEngine* mainEngine)
 {
     this->mainEngine = mainEngine;
+    mavsdk::ConnectionResult connection_result = mavsdk.add_any_connection(connection_url);
+
+    if (connection_result != mavsdk::ConnectionResult::Success)
+    {
+        std::cerr << "Connection failed: " << connection_result << std::endl;
+        return;
+    }
+
+    std::cout << "Connection successful" << std::endl;
+
     this->running = true;
+
+    auto start = std::chrono::steady_clock::now();
+
+    while (true) {
+        if (!mavsdk.systems().empty()) {
+            for (const auto& sys : mavsdk.systems()) {
+                if (sys->has_autopilot()) {
+                    system = sys;
+                    break;
+                }
+            }
+        }
+
+        if (system) {
+            std::cout << "System discovered!" << std::endl;
+            break;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        auto now = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::seconds>(now - start).count();
+        if (duration > 30) { // Timeout after 30 seconds
+            std::cerr << "Timed out waiting for system" << std::endl;
+            return;
+        }
+    }
+
+    /*
+    auto telemetry = std::make_shared<mavsdk::Telemetry>(system);
+    auto action = std::make_shared<mavsdk::Action>(system);
+    auto offboard = std::make_shared<mavsdk::Offboard>(system);
+
+    // Wait for the drone to be ready
+    while (!telemetry->health_all_ok()) {
+        std::cout << "Waiting for the drone to be ready..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::duration<int>(1));
+    }
+
+    std::cout << "Connected and ready..." << std::endl;
+    */
 }
 
 void DroneEngine::Run()
@@ -49,5 +111,22 @@ void DroneEngine::Stop()
 void DroneEngine::Shutdown()
 {
     instance = nullptr;
+}
+
+void takeoff(std::shared_ptr<mavsdk::Action> action) {
+    auto result = action->arm();
+    if (result != mavsdk::Action::Result::Success) {
+        std::cerr << "Arming failed: " << result << std::endl;
+        return;
+    }
+
+    result = action->takeoff();
+    if (result != mavsdk::Action::Result::Success) {
+        std::cerr << "Takeoff failed: " << result << std::endl;
+        return;
+    }
+
+    // Wait for the drone to reach a stable hover
+    //std::this_thread::sleep_for(10s);
 }
 
--- InfiniTAM/Apps/InfiniTAM_drone/DroneEngine.h
+++ InfiniTAM/Apps/InfiniTAM_drone/DroneEngine.h
@@ -5,6 +5,7 @@
 #ifndef DRONEENGINE_H
 #define DRONEENGINE_H
 #include <cstddef>
+#include <mavsdk.h>
 
 
 namespace ITMLib
@@ -14,15 +15,23 @@ namespace ITMLib
 
 class DroneEngine
 {
+
+private:
     static DroneEngine* instance;
     ITMLib::ITMMainEngine *mainEngine = nullptr;
     bool running = true;
+    mavsdk::Mavsdk mavsdk;
+    std::shared_ptr<mavsdk::System> system;
+
+    std::string connection_url = "udpin://0.0.0.0:14550";
 
 public:
+    DroneEngine();
     static DroneEngine* Instance() {
         if (instance == nullptr) instance = new DroneEngine();
         return instance;
     }
+
 
     void Initialise(ITMLib::ITMMainEngine* mainEngine);
 
